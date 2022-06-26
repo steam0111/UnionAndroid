8c6f86d342b4cd9476028169ede0ccc658db1993
TimoninD <rand1kch@gmail.com>
2022-06-26 21:34:54 +0300

    Добавил роу квери на ном группы

--- union_sync_impl/src/main/java/com/example/union_sync_impl/dao/NomenclatureGroupDao.kt
+++ union_sync_impl/src/main/java/com/example/union_sync_impl/dao/NomenclatureGroupDao.kt
@@ -4,12 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RawQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.example.union_sync_impl.entity.NomenclatureGroupDb
 
 @Dao
 interface NomenclatureGroupDao {
-    @Query("SELECT * FROM nomenclature_group")
-    suspend fun getAll(): List<NomenclatureGroupDb>
+    @RawQuery
+    fun getAll(query: SupportSQLiteQuery): List<NomenclatureGroupDb>
 
     @Query("SELECT * FROM nomenclature_group WHERE id = :nomenclatureGroupId LIMIT 1")
     suspend fun getById(nomenclatureGroupId: String): NomenclatureGroupDb
