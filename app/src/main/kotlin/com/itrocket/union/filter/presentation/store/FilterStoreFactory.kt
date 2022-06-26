2b58096bf74b2fa03be4ac3d20e316108dbc4642
Stanislav <steam0111@mail.ru>
2022-06-26 23:52:34 +0300

    Фикс краша

--- app/src/main/kotlin/com/itrocket/union/filter/presentation/store/FilterStoreFactory.kt
+++ app/src/main/kotlin/com/itrocket/union/filter/presentation/store/FilterStoreFactory.kt
@@ -9,7 +9,6 @@ import com.itrocket.core.base.BaseExecutor
 import com.itrocket.core.base.CoreDispatchers
 import com.itrocket.union.error.ErrorInteractor
 import com.itrocket.union.filter.domain.FilterInteractor
-import com.itrocket.union.manual.LocationParamDomain
 import com.itrocket.union.manual.ManualType
 import com.itrocket.union.manual.ParamDomain
 import com.itrocket.union.manual.Params
@@ -107,7 +106,10 @@ class FilterStoreFactory(
 
         private fun showFilters(filter: ParamDomain, getState: () -> FilterStore.State) {
             when (filter.type) {
-                ManualType.LOCATION -> publish(FilterStore.Label.ShowLocation(filter as LocationParamDomain))
+                ManualType.LOCATION -> {
+                    //no-op
+                    //publish(FilterStore.Label.ShowLocation(filter as LocationParamDomain))
+                }
                 ManualType.DATE -> {
                     //no-op
                 }
