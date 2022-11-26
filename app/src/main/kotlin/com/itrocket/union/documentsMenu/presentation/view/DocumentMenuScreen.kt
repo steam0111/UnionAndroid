1f746c7d831cd4754dccc75472969189d6de6c33
Stanislav <steam0111@mail.ru>
2022-11-26 19:38:03 +0000

    Task 147 Выровнять надпись "Типы имущества" под иконкой. Сейчас надпись смещена влево

--- app/src/main/kotlin/com/itrocket/union/documentsMenu/presentation/view/DocumentMenuScreen.kt
+++ app/src/main/kotlin/com/itrocket/union/documentsMenu/presentation/view/DocumentMenuScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -209,7 +210,8 @@ private fun DocumentItem(
         Text(
             text = stringResource(documentMenuItem.titleId),
             style = AppTheme.typography.subtitle2,
-            modifier = Modifier.padding(top = 12.dp)
+            modifier = Modifier.padding(top = 12.dp),
+            textAlign = TextAlign.Center
         )
     }
 }
